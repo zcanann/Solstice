9e4a3079f347d87ad1a0dc07c07f93fad6fd1494
Zachary Canann <zacharycanann@gmail.com>
2021-10-02 02:39:49 -0700

    Improve level up vfx

--- Data/Scripts/LevelUpFXController_C.lua
+++ Data/Scripts/LevelUpFXController_C.lua
@@ -3,38 +3,44 @@ local Framework = require(script:GetCustomProperty("Framework"))
 local propLevelUpVFXTemplate = script:GetCustomProperty("LevelUpVFX")
 local propLevelUpSFXTemplate = script:GetCustomProperty("LevelUpSFX")
 
+-- Mappings for color for level up effects.
+-- Improvements would be choosing different spiral/ground colors, and ensuring all abilities are unqiue visually.
 local vfxColorTable =
 {
     [ Framework.ExpTable.SkillMap.Attack ] = Color.New(1.0, 1.0, 0.0),
     [ Framework.ExpTable.SkillMap.Strength ] = Color.New(1.0, 1.0, 0.0),
     [ Framework.ExpTable.SkillMap.Defense ] = Color.New(1.0, 1.0, 0.0),
-    [ Framework.ExpTable.SkillMap.Health ] = "health",
-    [ Framework.ExpTable.SkillMap.Archery ] = "archery",
-    [ Framework.ExpTable.SkillMap.Magic ] = "magic",
-    [ Framework.ExpTable.SkillMap.Soul ] = "soul",
+    [ Framework.ExpTable.SkillMap.Health ] = Color.New(1.0, 1.0, 0.0),
+    [ Framework.ExpTable.SkillMap.Archery ] = Color.New(0.0, 1.0, 0.0),
+    [ Framework.ExpTable.SkillMap.Magic ] = Color.New(0.0, 0.0, 1.0),
+    [ Framework.ExpTable.SkillMap.Soul ] = Color.New(1.0, 1.0, 1.0),
     [ Framework.ExpTable.SkillMap.Slayer ] = Color.New(1.0, 0.0, 0.0),
-    [ Framework.ExpTable.SkillMap.Alchemy ] = "alchemy",
-    [ Framework.ExpTable.SkillMap.Mining ] = "mining",
-    [ Framework.ExpTable.SkillMap.Blacksmithing ] = "blacksmithing",
-    [ Framework.ExpTable.SkillMap.JewelCrafting ] = "jewelcrafting",
-    [ Framework.ExpTable.SkillMap.WoodCutting ] = "woodcutting",
-    [ Framework.ExpTable.SkillMap.WoodWorking ] = "woodworking",
-    [ Framework.ExpTable.SkillMap.Cooking ] = "cooking",
-    [ Framework.ExpTable.SkillMap.Tailoring ] = "tailoring",
-    [ Framework.ExpTable.SkillMap.LeatherWorking ] = "leatherworking",
-    [ Framework.ExpTable.SkillMap.Fishing ] = "fishing",
-    [ Framework.ExpTable.SkillMap.Enchanting ] = "enchanting",
-    [ Framework.ExpTable.SkillMap.Adventuring ] = "adventuring",
-    [ Framework.ExpTable.SkillMap.Botany ] = "botany",
-    [ Framework.ExpTable.SkillMap.Theiving ] = "theiving",
-    [ Framework.ExpTable.SkillMap.Agility ] = "agility",
-    [ Framework.ExpTable.SkillMap.Engineering ] = "engineering",
+    [ Framework.ExpTable.SkillMap.Alchemy ] = Color.New(0.5, 0.0, 0.5),
+    [ Framework.ExpTable.SkillMap.Mining ] = Color.New(0.5, 0.5, 0.5),
+    [ Framework.ExpTable.SkillMap.Blacksmithing ] = Color.New(0.5, 0.5, 0.5),
+    [ Framework.ExpTable.SkillMap.JewelCrafting ] = Color.New(0.5, 1.0, 1.0),
+    [ Framework.ExpTable.SkillMap.WoodCutting ] = Color.New(0.5, 0.25, 0.15),
+    [ Framework.ExpTable.SkillMap.WoodWorking ] = Color.New(0.5, 0.25, 0.15),
+    [ Framework.ExpTable.SkillMap.Cooking ] = Color.New(1.0, 0.85, 0.75),
+    [ Framework.ExpTable.SkillMap.Tailoring ] = Color.New(1.0, 0.85, 0.75),
+    [ Framework.ExpTable.SkillMap.LeatherWorking ] = Color.New(0.5, 0.25, 0.15),
+    [ Framework.ExpTable.SkillMap.Fishing ] = Color.New(0.0, 0.5, 1.0),
+    [ Framework.ExpTable.SkillMap.Enchanting ] = Color.New(0.5, 0.0, 0.5),
+    [ Framework.ExpTable.SkillMap.Adventuring ] = Color.New(1.0, 0.85, 0.75),
+    [ Framework.ExpTable.SkillMap.Botany ] = Color.New(0.0, 1.0, 0.0),
+    [ Framework.ExpTable.SkillMap.Theiving ] = Color.New(0.5, 0.5, 0.5),
+    [ Framework.ExpTable.SkillMap.Agility ] = Color.New(0.5, 0.5, 0.5),
+    [ Framework.ExpTable.SkillMap.Engineering ] = Color.New(0.5, 0.5, 0.5),
 }
 
 function OnPlayerLeveledUp(player, skillId)
     local vfx = World.SpawnAsset(propLevelUpVFXTemplate)
-    vfx:SetSmartProperty("Color", Color.New(1.0, 0.0, 0.0))
     vfx.lifeSpan = 5.0
+    if vfxColorTable[skillId] then
+        vfx:SetSmartProperty("Color", vfxColorTable[skillId])
+        vfx:SetSmartProperty("Spiral Color", vfxColorTable[skillId])
+        vfx:SetSmartProperty("Ground Element Color", vfxColorTable[skillId])
+    end
 	vfx:AttachToPlayer(player, "pelvis")
     vfx:SetPosition(Vector3.New(0.0, 0.0, -128.0))
     vfx:Play()
